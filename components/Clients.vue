496c90e1c5d6f771f588c355430f01f04f0ba586
Samuel <>
2022-03-14 20:31:50 +0000

    services and clients bar done (service bar has same color due to class error)

--- components/Clients.vue
+++ components/Clients.vue
@@ -1,128 +1,66 @@
 <template>
 	<div>
-<section
-      class="features2 woodm4_features2 cid-sVVvu2V6i8"
-      id="features02-8p"
-    >
-      <div class="container">
-        <div class="row">
-          <div class="title col-12 pb-4 align-center">
-            <h2 class="pb-3 mbr-bold mbr-fonts-style display-2">Clients</h2>
-          </div>
-        </div>
-      </div>
+		<section
+			class="features2 woodm4_features2 cid-sVVvu2V6i8"
+			id="features02-8x"
+		>
+			<div class="container">
+				<div class="row">
+					<div class="title col-12 pb-4 align-center">
+						<h2 class="pb-3 mbr-bold mbr-fonts-style display-2">
+							&nbsp; Clients
+						</h2>
+					</div>
+				</div>
+			</div>
 
-      <div class="container">
-        <div class="row">
-          <div class="card align-center col-12 col-md-6 col-lg-4">
-            <div class="panel-item">
-              <div class="card-block">
-                <div class="text-wrap">
-                  <div class="link-wrap pt-3">
-                    <h4
-                      class="link align-center mbr-bold mbr-black mbr-fonts-style display-7"
-                    >
-                      <a href="town-houses.html" class="text-primary"
-                        ><strong>Town Houses</strong></a
-                      ><br />
-                    </h4>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="card align-center col-12 col-md-6 col-lg-4">
-            <div class="panel-item">
-              <div class="card-block">
-                <div class="text-wrap">
-                  <div class="link-wrap pt-3">
-                    <h4
-                      class="link align-center mbr-bold mbr-black mbr-fonts-style display-7"
-                    >
-                      <a href="holiday-properties.html" class="text-primary"
-                        ><strong>Holiday Properties</strong></a
-                      >
-                    </h4>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="card align-center col-12 col-md-6 col-lg-4">
-            <div class="panel-item">
-              <div class="card-block">
-                <div class="text-wrap">
-                  <div class="link-wrap pt-3">
-                    <h4
-                      class="link align-center mbr-bold mbr-black mbr-fonts-style display-7"
-                    >
-                      <a href="commercial-properties.html" class="text-primary"
-                        ><strong>Commercial Properties</strong></a
-                      >
-                    </h4>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="card align-center col-12 col-md-6 col-lg-4">
-            <div class="panel-item">
-              <div class="card-block">
-                <div class="text-wrap">
-                  <div class="link-wrap pt-3">
-                    <h4
-                      class="link align-center mbr-bold mbr-black mbr-fonts-style display-7"
-                    >
-                      <a href="large-estates.html" class="text-primary"
-                        ><strong>Large Estates</strong></a
-                      >
-                    </h4>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="card align-center col-12 col-md-6 col-lg-4">
-            <div class="panel-item">
-              <div class="card-block">
-                <div class="text-wrap">
-                  <div class="link-wrap pt-3">
-                    <h4
-                      class="link align-center mbr-bold mbr-black mbr-fonts-style display-7"
-                    >
-                      <a href="page16.html" class="text-primary"
-                        ><strong>Allotment Holders</strong></a
-                      >
-                    </h4>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="card align-center col-12 col-md-6 col-lg-4">
-            <div class="panel-item">
-              <div class="card-block">
-                <div class="text-wrap">
-                  <div class="link-wrap pt-3">
-                    <h4
-                      class="link align-center mbr-bold mbr-black mbr-fonts-style display-7"
-                    >
-                      <a href="rural-land-owners.html" class="text-primary"
-                        ><strong>Rural Land Owners</strong></a
-                      ><br />
-                    </h4>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
+			<div class="container">
+				<div class="row">
+					<div
+						v-for="(item, index) in data"
+						:key="index"
+						class="card align-center col-12 col-md-6 col-lg-4"
+					>
+						<nuxt-link class="text-primary" :to="'/clients/' + item.slug">
+							<img
+								height="50"
+								:src="$store.state.assetRoot + item.icon.path"
+								alt=""
+							/>
+							<div class="panel-item">
+								<div class="card-block">
+									<div class="text-wrap">
+										<div class="link-wrap pt-3">
+											<h4
+												class="
+													link
+													align-center
+													mbr-bold mbr-black mbr-fonts-style
+													display-7
+												"
+											>
+												<strong>
+													<b> {{ item.name }}</b></strong
+												>
+											</h4>
+										</div>
+									</div>
+								</div>
+							</div>
+						</nuxt-link>
+					</div>
+				</div>
+			</div>
+		</section>
 	</div>
 </template>
+
+<script>
+export default {
+	data() {
+		return {
+			data: this.$store.state.staticData[3][0],
+		};
+	},
+};
+</script>
--- components/Services.vue
+++ components/Services.vue
@@ -1,139 +1,66 @@
 <template>
-  <div>
-    <section
-      class="features2 woodm4_features2 cid-sXeGR1VsH5"
-      id="features02-8x"
-    >
-      <div class="container">
-        <div class="row">
-          <div class="title col-12 pb-4 align-center">
-            <h2 class="pb-3 mbr-bold mbr-fonts-style display-2">
-              &nbsp; Services
-            </h2>
-          </div>
-        </div>
-      </div>
+	<div>
+		<section
+			class="features2 woodm4_features2 cid-sVVvu2V6i8"
+			id="features02-8x"
+		>
+			<div class="container">
+				<div class="row">
+					<div class="title col-12 pb-4 align-center">
+						<h2 class="pb-3 mbr-bold mbr-fonts-style display-2">
+							&nbsp; Services
+						</h2>
+					</div>
+				</div>
+			</div>
 
-      <div class="container">
-        <div class="row">
-          <div class="card align-center col-12 col-md-6 col-lg-4">
-            <div class="panel-item">
-              <div class="card-block">
-                <div class="text-wrap">
-                  <div class="link-wrap pt-3">
-                    <h4
-                      class="link align-center mbr-bold mbr-black mbr-fonts-style display-7"
-                    >
-                      <strong>
-                        <a
-                          href="regular-gardening-services.html"
-                          class="text-primary"
-                        >
-                          <b>Regular Garden&nbsp;Service</b></a
-                        >
-                      </strong>
-                    </h4>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="card align-center col-12 col-md-6 col-lg-4">
-            <div class="panel-item">
-              <div class="card-block">
-                <div class="text-wrap">
-                  <div class="link-wrap pt-3">
-                    <h4
-                      class="link align-center mbr-bold mbr-black mbr-fonts-style display-7"
-                    >
-                      <a href="garden-troubleshooting.html" class="text-primary"
-                        ><strong>Garden Troubleshooting</strong></a
-                      ><br />
-                    </h4>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="card align-center col-12 col-md-6 col-lg-4">
-            <div class="panel-item">
-              <div class="card-block">
-                <div class="text-wrap">
-                  <div class="link-wrap pt-3">
-                    <h4
-                      class="link align-center mbr-bold mbr-black mbr-fonts-style display-7"
-                    >
-                      <a href="planting-and-growing.html" class="text-primary"
-                        ><strong>Planting and Growing</strong></a
-                      >
-                    </h4>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="card align-center col-12 col-md-6 col-lg-4">
-            <div class="panel-item">
-              <div class="card-block">
-                <div class="text-wrap">
-                  <div class="link-wrap pt-3">
-                    <h4
-                      class="link align-center mbr-bold mbr-black mbr-fonts-style display-7"
-                    >
-                      <a href="minor-tree-surgery.html" class="text-primary"
-                        ><strong>Minor Tree Surgery&nbsp;</strong></a
-                      ><br />
-                    </h4>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="card align-center col-12 col-md-6 col-lg-4">
-            <div class="panel-item">
-              <div class="card-block">
-                <div class="text-wrap">
-                  <div class="link-wrap pt-3">
-                    <h4
-                      class="link align-center mbr-bold mbr-black mbr-fonts-style display-7"
-                    >
-                      <strong
-                        ><a href="allotment-care.html" class="text-primary"
-                          ><b>Allotment&nbsp;Care</b></a
-                        ></strong
-                      >
-                    </h4>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="card align-center col-12 col-md-6 col-lg-4">
-            <div class="panel-item">
-              <div class="card-block">
-                <div class="text-wrap">
-                  <div class="link-wrap pt-3">
-                    <h4
-                      class="link align-center mbr-bold mbr-black mbr-fonts-style display-7"
-                    >
-                      <a
-                        href="general-property-maintenance.html"
-                        class="text-primary"
-                        ><strong>Property Maintenance</strong></a
-                      >
-                    </h4>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-  </div>
+			<div class="container">
+				<div class="row">
+					<div
+						v-for="(item, index) in data"
+						:key="index"
+						class="card align-center col-12 col-md-6 col-lg-4"
+					>
+						<nuxt-link class="text-primary" :to="'/services/' + item.slug">
+							<img
+								height="50"
+								:src="$store.state.assetRoot + item.icon.path"
+								alt=""
+							/>
+							<div class="panel-item">
+								<div class="card-block">
+									<div class="text-wrap">
+										<div class="link-wrap pt-3">
+											<h4
+												class="
+													link
+													align-center
+													mbr-bold mbr-black mbr-fonts-style
+													display-7
+												"
+											>
+												<strong>
+													<b> {{ item.name }}</b></strong
+												>
+											</h4>
+										</div>
+									</div>
+								</div>
+							</div>
+						</nuxt-link>
+					</div>
+				</div>
+			</div>
+		</section>
+	</div>
 </template>
+
+<script>
+export default {
+	data() {
+		return {
+			data: this.$store.state.staticData[0][0],
+		};
+	},
+};
+</script>
